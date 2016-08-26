510d80fa5577dbe9a09b3129727927dc4e6905e4
Spleans <spleans@mt2015.com>
2016-08-26 21:38:26 +0300

    Created MainActivity

---
+++ app/src/main/java/com/spleans/remindme2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.spleans.remindme2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Spleans on 2016.08.26.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
