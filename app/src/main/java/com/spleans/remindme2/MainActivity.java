38ec330801690650a750ae05c3358e361dd5c0d4
Spleans <spleans@mt2015.com>
2016-08-30 23:05:45 +0300

    Added Toolbar and search button

--- app/src/main/java/com/spleans/remindme2/MainActivity.java
+++ app/src/main/java/com/spleans/remindme2/MainActivity.java
@@ -2,16 +2,38 @@ package com.spleans.remindme2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-/**
- * Created by Spleans on 2016.08.26.
- */
+
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
     }
+
+    private void initToolbar() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+
+
+
+    }
+
 }
 
